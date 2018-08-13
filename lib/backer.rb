ab2a2b8990252b9b28b9e96cd4cde1a9e3a2af28
Michael Roose <mjroose@gmail.com>
2018-08-13 18:42:55 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,10 @@
 class Backer
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     self.name = name
+    self.backed_projects = []
   end
+
+  def back_project(project)
 end
